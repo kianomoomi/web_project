b92a1e2bba95d2e8731dfeeda2bb875766ad41d7
kianomoomi <kianomoomi1380@gmail.com>
2022-02-18 04:31:03 +0330

    create user

---
+++ User.js
@@ -0,0 +1,10 @@
+const user = {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    img: {type: String}
+};
